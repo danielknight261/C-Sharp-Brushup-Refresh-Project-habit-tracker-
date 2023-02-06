9e7e40aa04b08f6a2d3d43d137d4f5a114a68fba
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-02-06 17:01:45 +0000

    update

--- habit-tracker/Program.cs
+++ habit-tracker/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace habit_tracker
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        string connectingString = @"Data Source=habit-Tracker.db"        
+
+        using (var connection = new SqliteConnection(connectingString)) {
+            connection.Open();
+            var tableCmd = connection.CreateCommand
+
+            tableCmd.CommandText = "";
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            }
+
+        }
+    }
+
+}
+
